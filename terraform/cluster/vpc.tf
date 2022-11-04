ba46cd231deb11101f70c96dbe09b9665765d1ff
jgreenberglw <114448847+jgreenberglw@users.noreply.github.com>
2022-11-04 16:06:43 -0700

    change DNS

--- terraform/cluster/vpc.tf
+++ terraform/cluster/vpc.tf
@@ -15,7 +15,7 @@ module "vpc" {
   public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   enable_nat_gateway   = true
   single_nat_gateway   = true
-  enable_dns_hostnames = true
+  enable_dns_hostnames = false
 
   tags = {
     "kubernetes.io/cluster/${var.env_name}" = "shared"
